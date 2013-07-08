3915477f3f2d5a429e40a091f04c2cdca477740c
Ayumi Yu <ayumi@ayumiyu.com>
2013-07-07 19:49:09 -0700

    Fresh Rails 4.0
    so fresh and so clean

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubynotifier::Application.config.secret_key_base = '07e1da3bfc6af6bbb58700cf18869b534e530179e47effc7884814d7f3804c881ecdb55840cb9f211cf25f79d18c096f7c43add6542055690496c913c63c29f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rubynotifier::Application.config.session_store :cookie_store, key: '_rubynotifier_session'
