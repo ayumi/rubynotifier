4cbe819e80ab4b6dbce295907477a7bf9161e337
Ayumi Yu <ayumi@ayumiyu.com>
2013-07-31 18:56:02 -0700

    puma config for on the fly worker/thread scaling

--- config/puma.rb
+++ config/puma.rb
@@ -1,2 +1,9 @@
-workers 5
-threads 1,1
+workers Integer(ENV["PUMA_WORKERS"] || 5)
+threads Integer(ENV["PUMA_THREADS_MIN"] || 1), Integer(ENV["PUMA_THREADS"] || 1)
+preload_app!
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
