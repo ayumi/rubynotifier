63751879f4691d4b1019328331db985f53ae217c
Ayumi Yu <ayumi@ayumiyu.com>
2013-07-31 23:27:12 -0700

    Fix host name for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rubynotifier::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: "www.rubynnotifier.org" }
+  config.action_mailer.default_url_options = { host: "www.rubynotifier.org" }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
